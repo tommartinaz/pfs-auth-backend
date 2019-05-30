6626712f49acea572f73c3d700de3f211b9ab8fe
Tom Martin <tom@Poe.local>
2019-05-30 06:39:17 -0700

    set up bcrypt to hash passwords as player is created. Working, sort of. Still get 404 error on post /signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
 const jwt = require('jwt-simple');
+const uuidv4 = require('uuid');
+const bcrypt = require('bcrypt-nodejs');
 const config = require('../config');
 const knex = require('../db/knex');
-const uuidv4 = require('uuid');
 
 const tokenForUser = user => {
     const timestamp = new Date().getTime();
     return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
-
 module.exports = {
     signup(req, res, next) {
         const { email, password } = req.body;
@@ -20,11 +20,27 @@ module.exports = {
                 if(players.length) {
                     return res.status(422).send({ error: 'Email is already in use' });
                 } else {
-                    knex('players')
-                        .insert({ ...req.body, id: uuidv4() }, '*')
-                        .then(players => {
-                            res.status(201).json({ id: players[0].id, token: tokenForUser(players[0]) })
+                    bcrypt.genSalt(10, (err, salt) => {
+                        if(err) {
+                            return next(err);
+                        }
+
+                        bcrypt.hash(req.body.password, salt, null, (err, hash) => {
+                            if(err) {
+                                return next(err);
+                            }
+                            // player = { ...req.body, password: hash, id: uuidv4() };
+                            knex('players')
+                                .insert({ ...req.body, password: hash, id: uuidv4() }, '*')
+                                .then(
+                                    // players => {
+                                    //     res.status(201).json({ id: players[0].id, token: tokenForUser(players[0]) })
+                                    // }
+                                    () => res.redirect('/')
+                                );
+                            next();
                         });
+                    });
                 }
             });
     },
